8a3ac4c6466aeff9361c93cf3fdce8c480be38a8
dwax1324 <dwax1324@gmail.com>
2023-12-13 20:38:43 +0900

    feat: 메인화면 출력기능 추가

---
+++ src/main/java/pairmatching/controller/Controller.java
@@ -0,0 +1,20 @@
+package pairmatching.controller;
+
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        outputView.printMainScreen();
+        String input = inputView.read();
+    }
+}
